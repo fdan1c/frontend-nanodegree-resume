31bf9ec9c92de67b5cb9902b9d2081d4780131d7
idanilov <fdan1c@gmail.com>
2015-01-22 23:12:14 +0300

    resume started

---
+++ js/resumeBuilder.js
@@ -0,0 +1,7 @@
+var name = "Igor Danilov";
+var role = "Business/System Analyst";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
